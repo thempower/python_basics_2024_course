0229086013bf72b2a0a596e7126c00fc582394d0
Krzysztof <krzysiek.staszewski@gmail.com>
2024-08-14 18:07:22 +0200

    finished exercises 1 and 2

---
+++ 13_excercises.py
@@ -0,0 +1,41 @@
+#DONE #1. Napisz program ktory sprawdza czy podana przez uzytkownika liczba jest parzysta czy nie parzysta - Write a program which validating of provided number by user is a integer or not
+
+# print("We will check or provided number  is a integer ")
+
+# user_number= int(input("Please provide a number "))
+# y = 2
+
+# if user_number % y != 0:
+#     print("Provided number not integer")
+# else:
+#     print("Provided number is integer")
+
+#DONE#2. Napisz program ktory sprawdza czy podana przez uzytkownika liczba jest wieszka, mniejsza lub równa zero
+print("Program will check of provided number is > 0 or = 0 or < 0 ")
+number = int(input("Please provide a number you want to check "))
+
+if number > 0:
+    print("Provided number is > 0")
+elif number < 0:
+    print("Provided number is < 0")
+else: 
+    print("Provided number is = 0")
+
+
+
+#3. Napisz program ktory zapyta uzytkownika o wynik od wynik na egazminie i wyswietli konkretna ocene
+# 90 - 100 -> 5
+# 80 - 89 -> 4
+# 70 - 79 -> 3
+# 60 - 69 -> 2
+# ponizej 60 -> 1
+#4 Napisz program ktory wyswietli liczby od 0 do 100
+#5 Napisz program ktory wyswietli wszystkie liczby pierwsze od 1 do 100
+#6 Napisz program ktory wyswietli sume wszystkich liczb parzystych z przedzialu 1 - 100
+#7 Napisz program ktory policzy pole prostokata (uzytkownik mus podac dlugosci bokow)
+#8 Napisz program ktory sprawdzi czy podane imie jest imieniem meskim, czy zenskim(zaloz ze imiona zenskie koncza sie na litere a)
+#9 Pobierz od uzytkownika 3 liczby calkowite i uporzadku jej w kolejnosc od najmniejsze do najwiekszej
+#10 Stworz gre w ktorej wylosuje liczbe z przedzialu 1-100 zapisze te liczbe w  zmiennej, a nastepnie poprosis uzytkownika o odgadniecie tej liczby. 
+#po kazdej proble wyswietlaj czy podana liczba jest mnieejsza czy wieksza od wylosowanej
+#Gdy uzytkownik odgadnie liczbe zakoncz gre
+#Znajdz informacje w jaki sposob losowac liczby calkowite w pythonie
